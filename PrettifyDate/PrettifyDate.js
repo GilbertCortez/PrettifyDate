0e3966d37a2e79cda91fa5a19d1c83338c72ed05
Gilbert Critica Cortez <gilbert230709@gmail.com>
2018-05-19 09:22:16 +0800

    Update PrettifyDate.js

--- PrettifyDate/PrettifyDate.js
+++ PrettifyDate/PrettifyDate.js
@@ -1,6 +1,6 @@
-function MMMMDDYYYY(i){
+function MMMMDDYYYY(i){ //this function accepts an Complete ISO-8601 date('YYYY-MM-DDT00:00:00.000Z')
                          var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
                          var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
                          var now = new Date(i);
-                         return months[now.getMonth()] + ' ' + now.getDate() + ', ' + now.getFullYear();
+                         return months[now.getMonth()] + ' ' + now.getDate() + ', ' + now.getFullYear(); //returns 'Month Day, Year' date format
                        }
